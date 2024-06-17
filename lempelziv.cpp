e7277460a47c5a416a9cb231371d2d074f70f28a
lulunkaii <sofyiaolate@gmail.com>
2024-06-17 18:03:54 -0400

    fix: Se sacan los metodos length de los bucles para optimizar el código. Se ordena la documentación de la clase lempelziv.cpp

--- lempelziv.cpp
+++ lempelziv.cpp
@@ -3,7 +3,8 @@
 #include "lempelziv.h"
 
 std::vector<int> lempelziv::comprimir(std::string input){  
-    std::unordered_map<std::string, int> tablaCodigos; // Crear una tabla para almacenar los códigos y sus correspondientes cadenas  
+    std::unordered_map<std::string, int> tablaCodigos;  // Crear una tabla para almacenar los códigos y sus
+                                                        // correspondientes cadenas  
     // Inicializar la tabla con los caracteres ASCII
     for (int i = 0; i <= 255; i++) { 
         std::string caracter = ""; 
@@ -13,24 +14,27 @@ std::vector<int> lempelziv::comprimir(std::string input){
     
     // Inicializar las variables para la compresión
     std::string cadenaActual = "", cadenaSiguiente = ""; 
-    cadenaActual += input[0]; // Iniciar cadenaActual con el primer carácter de la cadena
-    int code = 256; // Iniciar el código para las nuevas entradas en la tabla
-    std::vector<int> codigoSalida; // Vector para almacenar los códigos de salida
+    cadenaActual += input[0];               // Iniciar cadenaActual con el primer carácter de la cadena
+    int code = 256;                         // Iniciar el código para las nuevas entradas en la tabla
+    std::vector<int> codigoSalida;          // Vector para almacenar los códigos de salida
+    int cadenaLength = input.length();
 
     // Iterar sobre la cadena de texto para comprimir
-    for (int i = 0; i < input.length(); i++) { 
+    for (int i = 0; i < cadenaLength; i++) { 
         // Si no estamos al final de la cadena, añadir el siguiente carácter a cadenaSiguiente
-        if (i != input.length() - 1) 
+        if (i != cadenaLength - 1) 
             cadenaSiguiente += input[i + 1]; 
-        // Si la cadena cadenaActual+cadenaSiguiente está en la tabla, actualizar cadenaActual a cadenaActual+cadenaSiguiente
+        // Si la cadena cadenaActual+cadenaSiguiente está en la tabla, actualizar cadenaActual
+        // a cadenaActual+cadenaSiguiente
         if (tablaCodigos.find(cadenaActual + cadenaSiguiente) != tablaCodigos.end()) { 
             cadenaActual = cadenaActual + cadenaSiguiente; 
         } else { 
-            // Si no, añadir el código de cadenaActual a la salida y añadir cadenaActual+cadenaSiguiente a la tabla con un nuevo codigo
+            // Si no, añadir el código de cadenaActual a la salida y añadir cadenaActual+cadenaSiguiente
+            // a la tabla con un nuevo codigo
             codigoSalida.push_back(tablaCodigos[cadenaActual]); 
             codigos.push_back({cadenaActual, tablaCodigos[cadenaActual]});
             tablaCodigos[cadenaActual + cadenaSiguiente] = code; 
-            code++; // Incrementar el código para la próxima entrada
+            code++;                         // Incrementar el código para la próxima entrada
             cadenaActual = cadenaSiguiente; // Actualizar cadenaActual a cadenaSiguiente
         } 
         cadenaSiguiente = ""; // Limpiar cadenaSiguiente para la próxima iteración
@@ -42,7 +46,8 @@ std::vector<int> lempelziv::comprimir(std::string input){
 } 
 
 std::string lempelziv::descomprimir(std::vector<int> codigoEntrada){
-    std::unordered_map<int, std::string> tablaCadenas; // Crear una tabla para almacenar los códigos y sus correspondientes cadenas
+    std::unordered_map<int, std::string> tablaCadenas;  // Crear una tabla para almacenar los códigos y
+                                                        // sus correspondientes cadenas
     
     // Inicializar la tabla con los caracteres ASCII
     for (int i = 0; i <= 255; i++) { 
@@ -55,24 +60,28 @@ std::string lempelziv::descomprimir(std::vector<int> codigoEntrada){
     int codigoAnterior = codigoEntrada[0], codigoActual; 
     std::string cadenaActual = tablaCadenas[codigoAnterior]; 
     std::string cadenaSiguiente = ""; 
-    cadenaSiguiente += cadenaActual[0]; // Iniciar cadenaSiguiente con el primer carácter de cadenaActual
-    std::string resultado = cadenaActual; // Iniciar la cadena de resultado con cadenaActual
-    int count = 256; // Iniciar el contador para los nuevos códigos
-    
+    cadenaSiguiente += cadenaActual[0];     // Iniciar cadenaSiguiente con el primer carácter de cadenaActual
+    std::string resultado = cadenaActual;   // Iniciar la cadena de resultado con cadenaActual
+    int count = 256;                        // Iniciar el contador para los nuevos códigos
+    int entradaSize = codigoEntrada.size();
     // Iterar sobre los códigos de entrada para descomprimir
-    for (int i = 0; i < codigoEntrada.size() - 1; i++) { 
+    for (int i = 0; i < entradaSize - 1; i++) { 
         codigoActual = codigoEntrada[i + 1]; // Leer el siguiente código de la entrada
         if (tablaCadenas.find(codigoActual) == tablaCadenas.end()) { 
             cadenaActual = tablaCadenas[codigoAnterior]; 
-            cadenaActual = cadenaActual + cadenaSiguiente; // Si codigoActual no está en la tabla, actualizar cadenaActual a la cadena correspondiente a codigoAnterior seguida de cadenaSiguiente
+            cadenaActual = cadenaActual + cadenaSiguiente;  // Si codigoActual no está en la tabla, actualizar 
+                                                            // cadenaActual a la cadena correspondiente a codigoAnterior
+                                                            // seguida de cadenaSiguiente
         }
         else { 
-            cadenaActual = tablaCadenas[codigoActual]; // Si codigoActual está en la tabla, actualizar cadenaActual a la cadena correspondiente a codigoActual
+            cadenaActual = tablaCadenas[codigoActual];  // Si codigoActual está en la tabla, actualizar cadenaActual
+                                                        // a la cadena correspondiente a codigoActual
         }
         resultado += cadenaActual; // Añadir cadenaActual a la cadena de resultado
         cadenaSiguiente = ""; 
         cadenaSiguiente += cadenaActual[0]; // Actualizar cadenaSiguiente al primer carácter de cadenaActual
-        tablaCadenas[count] = tablaCadenas[codigoAnterior] + cadenaSiguiente; // Añadir a la tabla la cadena correspondiente a codigoAnterior seguida de cadenaSiguiente con un nuevo código
+        // Añadir a la tabla la cadena correspondiente a codigoAnterior seguida de cadenaSiguiente con un nuevo código
+        tablaCadenas[count] = tablaCadenas[codigoAnterior] + cadenaSiguiente; 
         count++; // Incrementar el contador para el próximo código
         codigoAnterior = codigoActual; // Actualizar codigoAnterior a codigoActual
     }
